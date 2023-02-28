baa482a2bff721a91399d6327fbb14dad09a761f
Emmanuel Chukwu <Officialecho95@outlook.com>
2023-02-28 07:46:53 +0000

    all project headers

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+#include <stdio.h>
+/**
+ * binary_tree_node - function to create node
+ *@parent: pointer to root node
+ *@value: value at position
+ * Return:Your function must return a pointer to the new node or NULL if no.
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+
+	if (newNode == NULL)
+		return (NULL);
+	newNode->n = value;
+	newNode->parent = parent;
+	newNode->left = NULL;
+	newNode->right = NULL;
+
+	return (newNode);
+}
