aa3ea1a7737b2df05274db8d8cbb26cae757871a
Emmanuel Chukwu <Officialecho95@outlook.com>
2023-03-02 07:47:45 +0000

    height of binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,17 +7,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_left, height_right;
+	if (tree)
+	{
+		int left = 0, right = 0;
 
-	if (tree == NULL)
-		return (0);
-
-	height_left = binary_tree_height(tree->left);
-	height_right = binary_tree_height(tree->right);
-
-	if (height_left > height_right)
-		return (height_left + 1);
-	
+		if (tree->right)
+			right = 1 + binary_tree_height(tree->right);
+		if (tree->left)
+			left = 1 + binary_tree_height(tree->left);
+		if (left > right)
+			return (left);
+		else
+			return (right);
+	}
 	else
-		return (height_right + 1);
+		return (0);
 }
